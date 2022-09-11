085273553a4fc49513f209d6fa443b0a74dec8d6
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 15:20:42 -0400

    Update EmployeeService.java

--- mhl/service/EmployeeService.java
+++ mhl/service/EmployeeService.java
@@ -4,16 +4,15 @@ import mhl.dao.EmployeeDAO;
 import mhl.domain.Employee;
 
 /**
- * @author Carson
- * 该类完成对employee表的各种操作 （通过调用 EmployeeDao 对象完成）
+ * This class performs various operations on the employee table (by calling the EmployeeDao object)
  */
 public class EmployeeService {
 
-    // 定义一个EmployeeDao 属性
+    // Define an EmployeeDao property
     EmployeeDAO employeeDAO=  new EmployeeDAO();
 
-    // 根据empId和pwd返回一个Employee对象
-    // 如果查询不到，返回null
+    // Returns an Employee object based on empId and pwd
+    // If there is no this object, return null
     public Employee getEmployeeByIdAndPwd(String empId,String pwd){
         Employee employee = employeeDAO.querySingle("select * from employee where empId=? and pwd = md5(?)", Employee.class, empId, pwd);
         return employee;
