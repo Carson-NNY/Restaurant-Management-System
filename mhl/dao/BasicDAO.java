1e99642846dba66522e56964d481c80a591bb8e9
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 12:21:56 -0400

    Update BasicDAO.java

--- mhl/dao/BasicDAO.java
+++ mhl/dao/BasicDAO.java
@@ -11,14 +11,13 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
- * @author Carson
- * 开发BasicDAO， 是其他DAO的父类
+ * Develop BasicDAO, which is the parent class of other DAOs
  */
-public class BasicDAO<T> {  // 泛型指定具体类型
+public class BasicDAO<T> {  
 
-    private  QueryRunner qr =  new QueryRunner();   // 这里的代码需要提前导入jar包（commons-dbutils-1.3）
+    private  QueryRunner qr =  new QueryRunner();   // The code here needs to import the jar package in advance（commons-dbutils-1.3）
 
-    //开发通用的 dml 方法, 针对任意的表
+    //Develop generic dml methods for arbitrary tables
     public int update(String sql, Object... parameters){
         Connection connection = null;
 
@@ -28,17 +27,17 @@ public class BasicDAO<T> {  // 泛型指定具体类型
             return update;
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);   //将编译异常->运行异常,抛出. 好处： 调用者
-                                    // 可以选择捕获或者不处理它
+            throw new RuntimeException(e);   //make complie exception-> runtime exception, Benefit: The caller can choose to capture it or not process it
+
         }finally {
             JDBCUtilesByDruid.close(null,null,connection);
         }
     }
 
-    //返回多个对象(即查询的结果是多行), 针对任意表
-   // @param clazz 传入一个类的 Class 对象(底层需要知道这个类有哪些属性
-    // 通过反射创建javabean对象， 比如 Actor.class)
-    //  @param parameters 传入 ? 的具体的值，可以是多个值
+    //Returns multiple objects (that is, the result of the query is multiple rows), for any table
+   // @param clazz Pass in a class object (needs to know what properties the class has.)
+    // Create javabean objects through reflection, for example Actor.class)
+    //  @param parameters pass in the value of ?,   Can be multiple values
     public List<T>  queryMulti(String sql,Class<T> clazz,Object... parameters){
         Connection connection = null;
 
@@ -46,8 +45,7 @@ public class BasicDAO<T> {  // 泛型指定具体类型
             connection = JDBCUtilesByDruid.getConnection();
             return qr.query(connection, sql, new BeanListHandler<T>(clazz), parameters);
         } catch (SQLException e) {
-            throw new RuntimeException(e);   //将编译异常->运行异常,抛出. 好处： 调用者
-            // 可以选择捕获或者不处理它
+            throw new RuntimeException(e);  
         }finally {
             JDBCUtilesByDruid.close(null,null,connection);
         }
@@ -55,7 +53,7 @@ public class BasicDAO<T> {  // 泛型指定具体类型
     }
 
 
-    //查询单行结果 的通用方法
+    //A general method for querying single-row results
     public T querySingle(String sql, Class<T> clazz, Object... parameters) {
         Connection connection = null;
 
@@ -64,14 +62,13 @@ public class BasicDAO<T> {  // 泛型指定具体类型
             return qr.query(connection, sql, new BeanHandler<T>(clazz), parameters);
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);   //将编译异常->运行异常,抛出. 好处： 调用者
-            // 可以选择捕获或者不处理它
+            throw new RuntimeException(e);   
         }finally {
             JDBCUtilesByDruid.close(null,null,connection);
         }
     }
 
-    //查询单行单列的方法,即返回单值的方法
+    //The method of querying a single row and single column, that is, the method of returning a single value
     public Object queryScalar(String sql, Object... parameters) {
         Connection connection = null;
 
@@ -81,8 +78,7 @@ public class BasicDAO<T> {  // 泛型指定具体类型
 
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);   //将编译异常->运行异常,抛出. 好处： 调用者
-            // 可以选择捕获或者不处理它
+            throw new RuntimeException(e); 
         }finally {
             JDBCUtilesByDruid.close(null,null,connection);
         }
