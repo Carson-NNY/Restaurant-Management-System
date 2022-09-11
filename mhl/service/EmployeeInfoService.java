5600cd3fe14daf13e3164a0709d5e63a3cb2469f
Carson-NNY <109368070+Carson-NNY@users.noreply.github.com>
2022-09-11 15:18:17 -0400

    Update EmployeeInfoService.java

--- mhl/service/EmployeeInfoService.java
+++ mhl/service/EmployeeInfoService.java
@@ -3,10 +3,7 @@ package mhl.service;
 import mhl.dao.EmployeeInfoDAO;
 import mhl.domain.EmployeeInfo;
 
-/**
- * @author Carson
- * @Version
- */
+
 public class EmployeeInfoService {
     private EmployeeInfoDAO employeeInfoDAO= new EmployeeInfoDAO();
 
